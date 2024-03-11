d6708b1c9caf042ff672fd3b106f2c043cbcd61c
pablogch <pablochoconi@gmail.com>
2024-03-10 23:30:09 -0300

    Mob scene

--- .godot/mono/temp/obj/Debug/Squash the Creeps (3D).AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Squash the Creeps (3D).AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Squash the Creeps (3D)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a35287e419b57d2389336c23f9cddaed2b8a6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7afb051654e2f7880220295f15ab56ad594ed60b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Squash the Creeps (3D)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Squash the Creeps (3D)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
