984717d4be5f32cb2bd9da966c6b325904d96f39
pablogch <pablochoconi@gmail.com>
2024-03-11 00:05:35 -0300

    Main scene base script

--- .godot/mono/temp/obj/Debug/Squash the Creeps (3D).AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Squash the Creeps (3D).AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Squash the Creeps (3D)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7afb051654e2f7880220295f15ab56ad594ed60b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6708b1c9caf042ff672fd3b106f2c043cbcd61c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Squash the Creeps (3D)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Squash the Creeps (3D)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Scenes/Main/Main.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+  [Export]
+  public PackedScene MobScene { get; set; }
+
+  private Player _player;
+
+  private PathFollow3D _mobSpawnLocation;
+
+  public void OnMobTimerTimout()
+  {
+    // Create a new instance of the Mob scene.
+    Mob mob = MobScene.Instantiate<Mob>();
+
+    // Choose a random location on the SpawnPath.
+    // We store the reference to the SpawnLocation node.
+    // And give it a random offset.
+    _mobSpawnLocation.ProgressRatio = GD.Randf();
+
+    Vector3 playerPosition = _player.Position;
+    mob.Initialize(_mobSpawnLocation.Position, playerPosition);
+
+    // Spawn the mob by adding it to the Main scene.
+    AddChild(mob);
+  }
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+    _player = GetNode<Player>("Player");
+    _mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+	}
+}
