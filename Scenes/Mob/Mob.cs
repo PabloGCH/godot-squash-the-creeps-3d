4c8043cf1add2addfb00c088d68941aa3d531826
pablogch <pablochoconi@gmail.com>
2024-03-11 23:26:30 -0300

    Jumping and squashing

--- Scenes/Mob/Mob.cs
+++ Scenes/Mob/Mob.cs
@@ -1,39 +1,49 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class Mob : CharacterBody3D
 {
     [Export]
     public int MinSpeed { get; set; } = 10;
+
     // Maximum speed of the mob in meters per second
     [Export]
     public int MaxSpeed { get; set; } = 18;
+
+    [Signal]
+    public delegate void SquashedEventHandler();
 
     public override void _PhysicsProcess(double delta)
     {
         MoveAndSlide();
     }
-    
+
     public void OnVisibilityNotifierScreenExited()
     {
         QueueFree();
     }
 
+    public void Squash()
+    {
+        EmitSignal(SignalName.Squashed);
+        QueueFree();
+    }
+
     public void Initialize(Vector3 startPosition, Vector3 playerPosition)
     {
-      // We position the mob by placing it at startPosition
-      // and rotate it towards playerPosition, so it looks at the player.
-      LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
-      // Rotate this mob randomly within range of -45 and +45 degrees,
-      // so that it doesn't move directly towards the player.
-      RotateY((float)GD.RandRange(-Mathf.Pi / 4.0, Mathf.Pi / 4.0));
+        // We position the mob by placing it at startPosition
+        // and rotate it towards playerPosition, so it looks at the player.
+        LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
+        // Rotate this mob randomly within range of -45 and +45 degrees,
+        // so that it doesn't move directly towards the player.
+        RotateY((float)GD.RandRange(-Mathf.Pi / 4.0, Mathf.Pi / 4.0));
 
-      // We calculate a random speed (integer).
-      int randomSpeed = GD.RandRange(MinSpeed, MaxSpeed);
-      // We calculate a forward velocity that represents the speed.
-      Velocity = Vector3.Forward * randomSpeed;
-      // We then rotate the velocity vector based on the mob's Y rotation
-      // in order to move in the direction the mob is looking.
-      Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+        // We calculate a random speed (integer).
+        int randomSpeed = GD.RandRange(MinSpeed, MaxSpeed);
+        // We calculate a forward velocity that represents the speed.
+        Velocity = Vector3.Forward * randomSpeed;
+        // We then rotate the velocity vector based on the mob's Y rotation
+        // in order to move in the direction the mob is looking.
+        Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
     }
 }
--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -9,13 +9,19 @@ public partial class Player : CharacterBody3D
     [Export]
     public const float FallAcceleration = 75;
 
+    [Export]
+    public int JumpImpulse { get; set; } = 20;
+
+    [Export]
+    public int BounceImpulse { get; set; } = 16;
+
     private Vector3 _targetVelocity = Vector3.Zero;
 
     public override void _PhysicsProcess(double delta)
     {
         // We create a local variable to store the input direction.
         var direction = Vector3.Zero;
-        
+
         GD.Print("Player _PhysicsProcess");
 
         // We check for each move input and update the direction accordingly.
@@ -51,6 +57,34 @@ public partial class Player : CharacterBody3D
         if (!IsOnFloor()) // If in the air, fall towards the floor. Literally gravity
         {
             _targetVelocity.Y -= FallAcceleration * (float)delta;
+        }
+
+        if (IsOnFloor() && Input.IsActionJustPressed("jump"))
+        {
+            _targetVelocity.Y = JumpImpulse;
+        }
+
+        // Iterate through all collisions that occurred this frame.
+        for (int index = 0; index < GetSlideCollisionCount(); index++)
+        {
+            // We get one of the collisions with the player.
+            KinematicCollision3D collision = GetSlideCollision(index);
+
+            // If the collision is with a mob.
+            // With C# we leverage typing and pattern-matching
+            // instead of checking for the group we created.
+            if (collision.GetCollider() is Mob mob)
+            {
+                // We check that we are hitting it from above.
+                if (Vector3.Up.Dot(collision.GetNormal()) > 0.1f)
+                {
+                    // If so, we squash it and bounce.
+                    mob.Squash();
+                    _targetVelocity.Y = BounceImpulse;
+                    // Prevent further duplicate calls.
+                    break;
+                }
+            }
         }
 
         // Moving the character
